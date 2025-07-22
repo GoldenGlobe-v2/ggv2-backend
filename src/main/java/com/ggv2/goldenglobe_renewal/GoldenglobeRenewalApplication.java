2a2563554c9cdc89d834fe755ed2d16cc63442a4
Jaeyeong Won <devlog.jae13@gmail.com>
2025-07-22 17:31:40 +0900

    :tada: Initial Commit

---
+++ src/main/java/com/ggv2/goldenglobe_renewal/GoldenglobeRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.ggv2.goldenglobe_renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenglobeRenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenglobeRenewalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggv2/goldenglobe_renewal/GoldenglobeRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggv2.goldenglobe_renewal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenglobeRenewalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
