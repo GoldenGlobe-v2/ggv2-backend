e4d1f1ef1f3b5964db0360e3242f7afe07032f1c
Jaeyeong Won <devlog.jae13@gmail.com>
2025-08-01 17:33:39 +0900

    :sparkles: PdfList 엔티티 및 Repository 생성

---
+++ src/main/java/com/ggv2/goldenglobe_renewal/domain/pdfList/PdfList.java
@@ -0,0 +1,36 @@
+package com.ggv2.goldenglobe_renewal.domain.pdfList;
+
+import com.ggv2.goldenglobe_renewal.domain.travelList.TravelList;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PDF_목록")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PdfList {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String pdfName;
+
+  @Column
+  private String pdfPath;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "travel_list_id")
+  private TravelList travelList;
+
+  @Builder
+  public PdfList(String pdfName, String pdfPath, TravelList travelList) {
+    this.pdfName = pdfName;
+    this.pdfPath = pdfPath;
+    this.travelList = travelList;
+  }
+}
